87775d54c5dc29e66aab2e789c58023683d2c1d0
Vadym Petelskyi <vadbcl@outlook.com>
2022-03-30 21:00:07 +0300

    + StringUtil class

--- src/ua/hillel/task9/Main.java
+++ src/ua/hillel/task9/Main.java
@@ -2,6 +2,20 @@ package ua.hillel.task9;
 
 public class Main {
     public static void main(String[] args) {
-        
+        String symbolStr = "phenomenon";
+        System.out.println(StringUtil.findSymbolOccurance(symbolStr, 'n'));
+
+        String subStr1 = "Apollo";
+        String subStr2 = "Apple";
+        System.out.println(StringUtil.findWordPosition(subStr1, "pollo"));
+        System.out.println(StringUtil.findWordPosition(subStr2, "Plant"));
+
+        String reversedStr = "Hello";
+        System.out.println(StringUtil.stringReverse(reversedStr));
+
+        String palindromeStr1 = "ERE";
+        String palindromeStr2 = "Allo";
+        System.out.println(StringUtil.isPalindrome(palindromeStr1));
+        System.out.println(StringUtil.isPalindrome(palindromeStr2));
     }
 }
---
+++ src/ua/hillel/task9/StringUtil.java
@@ -0,0 +1,28 @@
+package ua.hillel.task9;
+
+public class StringUtil {
+    public static int findSymbolOccurance(String str, char symbol) {
+        char[] chars = str.toCharArray();
+        int times = 0;
+        for (char c : chars) {
+            if (c == symbol) {
+                times++;
+            }
+        }
+        return times;
+    }
+
+    public static int findWordPosition(String source, String target) {
+        return source.indexOf(target);
+    }
+
+    public static String stringReverse(String str) {
+        StringBuilder sb = new StringBuilder(str);
+        return sb.reverse().toString();
+    }
+
+    public static boolean isPalindrome(String str) {
+        StringBuilder sb = new StringBuilder(str);
+        return str.equals(sb.reverse().toString());
+    }
+}
