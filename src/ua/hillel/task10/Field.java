8c7e78b3da0fbe6c8c3c25289781550805585f16
Vadym Petelskyi <vadbcl@outlook.com>
2022-03-31 23:17:36 +0300

    Tictactoe imported

---
+++ src/ua/hillel/task10/Field.java
@@ -0,0 +1,19 @@
+package ua.hillel.task10;
+
+public class Field {
+    private static final char EMPTY_SYMBOL = '-';
+
+    private final char[][] data = {
+            {EMPTY_SYMBOL, EMPTY_SYMBOL, EMPTY_SYMBOL},
+            {EMPTY_SYMBOL, EMPTY_SYMBOL, EMPTY_SYMBOL},
+            {EMPTY_SYMBOL, EMPTY_SYMBOL, EMPTY_SYMBOL}
+    };
+
+    public char[][] getData() {
+        return data;
+    }
+
+    public void setValue(int h, int v, char ch) {
+        data[h][v] = ch;
+    }
+}
