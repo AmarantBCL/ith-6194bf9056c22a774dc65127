d12b0e24ddb0527186169ccf7d57ea65a5f5f70e
AmarantBCL <vadbcl@outlook.com>
2022-04-07 22:27:32 +0300

    Completed

--- src/ua/hillel/task11/Main.java
+++ src/ua/hillel/task11/Main.java
@@ -2,6 +2,16 @@ package ua.hillel.task11;
 
 public class Main {
     public static void main(String[] args) {
-        
+        String[][] arr = {
+                {"12", "4", "16", "8"},
+                {"TEST", "12", "2", "18"},
+                {"1", "13", "11", "9"},
+                {"6", "7", "12", "19"}
+        };
+        try {
+            System.out.println(ArrayValueCalculator.doCalc(arr));
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/ua/hillel/task11/MyArrayDataException.java
@@ -0,0 +1,11 @@
+package ua.hillel.task11;
+
+public class MyArrayDataException extends Exception {
+    public MyArrayDataException(String message) {
+        super(message);
+    }
+
+    public MyArrayDataException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
