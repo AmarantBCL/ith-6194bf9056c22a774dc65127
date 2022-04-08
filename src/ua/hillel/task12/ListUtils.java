15b1c31bec5d68d6743f95b9d518ba8b5092f1e0
Vadym Petelskyi <vadbcl@outlook.com>
2022-04-08 18:11:04 +0300

    First methods

---
+++ src/ua/hillel/task12/ListUtils.java
@@ -0,0 +1,36 @@
+package ua.hillel.task12;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ListUtils {
+    public static int countOccurrence(List<String> list, String str) {
+        int count = 0;
+        for (String s : list) {
+            if (str.equals(s)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static List<Integer> toList(int[] arr) {
+        List<Integer> list = new ArrayList<>();
+        for (int i : arr) {
+            list.add(i);
+        }
+        return list;
+    }
+
+    public static List<Integer> findUnique(List<Integer> list) {
+        List<Integer> uniqueList = new ArrayList<>();
+        for (Integer i : list) {
+            if (!uniqueList.contains(i)) {
+                uniqueList.add(i);
+            }
+        }
+        return uniqueList;
+    }
+}
--- src/ua/hillel/task12/Main.java
+++ src/ua/hillel/task12/Main.java
@@ -1,7 +1,18 @@
 package ua.hillel.task12;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        
+        List<String> wordList = Arrays.asList("cat", "dog", "rabbit", "dog", "horse", "dog", "dog",
+                "rabbit", "cat", "pig");
+        System.out.println(ListUtils.countOccurrence(wordList, "pig"));
+
+        System.out.println(ListUtils.toList(new int[]{1,2,3}));
+
+        List<Integer> intList = Arrays.asList(1, 8, 5, 7, 9, 1, 2, 4, 5, 2, 2, 6, 3, 0, 0);
+        System.out.println(ListUtils.findUnique(intList));
     }
 }
