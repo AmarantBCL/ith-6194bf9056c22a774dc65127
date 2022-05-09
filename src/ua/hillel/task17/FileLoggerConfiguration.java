a4e4d42f4a6d24a90c9dfd7a50a1d8a775f1b567
AmarantBCL <vadbcl@outlook.com>
2022-05-09 10:37:53 +0300

    Constants

--- src/ua/hillel/task17/FileLoggerConfiguration.java
+++ src/ua/hillel/task17/FileLoggerConfiguration.java
@@ -6,13 +6,16 @@ import java.nio.file.Path;
 import java.time.LocalDateTime;
 
 public class FileLoggerConfiguration extends LoggerConfiguration {
+    private static String FILE_PREFIX = "Log_";
+    private static String FILE_FORMAT = ".txt";
+
     public FileLoggerConfiguration(String pathName, LoggingLevel logLevel, long maxSize, String format) {
         super(pathName, logLevel, maxSize, format);
     }
 
     @Override
     public void createNewFile() {
-        String fileName = "Log_" + FILE_NAME_TIME_FORMAT.format(LocalDateTime.now()) + ".txt";
+        String fileName = FILE_PREFIX + FILE_NAME_TIME_FORMAT.format(LocalDateTime.now()) + FILE_FORMAT;
         try {
             setPath(Files.createFile(Path.of(getPathName(), fileName)));
         } catch (IOException e) {
