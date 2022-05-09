90449a730ef26a34f6513803266efc44f6b2a9b6
AmarantBCL <vadbcl@outlook.com>
2022-05-09 10:40:55 +0300

    Small fix

--- src/ua/hillel/task17/FileLoggerConfiguration.java
+++ src/ua/hillel/task17/FileLoggerConfiguration.java
@@ -6,8 +6,8 @@ import java.nio.file.Path;
 import java.time.LocalDateTime;
 
 public class FileLoggerConfiguration extends LoggerConfiguration {
-    private static String FILE_PREFIX = "Log_";
-    private static String FILE_FORMAT = ".txt";
+    private static final String FILE_PREFIX = "Log_";
+    private static final String FILE_FORMAT = ".txt";
 
     public FileLoggerConfiguration(String pathName, LoggingLevel logLevel, long maxSize, String format) {
         super(pathName, logLevel, maxSize, format);
