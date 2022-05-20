0cb0fbb686bf6e409c59889fa8c3c0c0686ce7df
AmarantBCL <vadbcl@outlook.com>
2022-05-20 17:26:45 +0300

    Air raid siren save

--- src/ua/hillel/task19/Main.java
+++ src/ua/hillel/task19/Main.java
@@ -1,9 +1,25 @@
 package ua.hillel.task19;
 
-public class Main {
-    public static void main(String[] args) throws InterruptedException {
-        PetrolStation station = new PetrolStation();
-        
-    }
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.locks.ReentrantLock;
 
+public class Main {
+    private static final int MIN_LITERS = 5;
+    private static final int MAX_LITERS = 50;
+
+    public static void main(String[] args) throws Exception {
+        PetrolStation station = new PetrolStation(50);
+
+        Random random = new Random();
+        for (int i = 0; i < 3; i++) {
+            Thread thread = new Thread(() -> {
+                station.doRefuel(random.nextInt(MAX_LITERS - MIN_LITERS) + MIN_LITERS);
+                System.out.println("PETROL STATION amount: " + station.getAmount());
+            });
+            //thread.start();
+        }
+    }
 }
--- src/ua/hillel/task19/PetrolStation.java
+++ src/ua/hillel/task19/PetrolStation.java
@@ -1,12 +1,68 @@
 package ua.hillel.task19;
 
+import java.util.Random;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 public class PetrolStation {
+    public static final int MAX_REFUELS_AT_TIME = 2;
     private static final int MIN_REFUEL_TIME = 3;
     private static final int MAX_REFUEL_TIME = 10;
-    private static final int MAX_REFUELS_AT_TIME = 3;
+
     private float amount;
+    private Random random = new Random();
+    private Semaphore semaphore = new Semaphore(MAX_REFUELS_AT_TIME);
+    private ReadWriteLock lock = new ReentrantReadWriteLock();
+
+    public PetrolStation(int amount) {
+        this.amount = amount;
+    }
+
+    public float getAmount() {
+        try {
+            lock.readLock().lock();
+            return amount;
+        } finally {
+            lock.readLock().unlock();
+        }
+    }
 
     public void doRefuel(float fuel) {
+        try {
+            semaphore.acquire();
+            if (notEnough(fuel)) {
+                System.out.println("CAN'T REFUEL!");
+                return;
+            }
+            int sec = random.nextInt((MAX_REFUEL_TIME - MIN_REFUEL_TIME)) + MIN_REFUEL_TIME;
+            System.out.println(sec + " seconds WAITING...");
+            Thread.sleep(sec * 1000);
+            System.out.println(fuel + " liters");
+            changeAmount(fuel);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            semaphore.release();
+        }
+    }
 
+    private void changeAmount(float fuel) {
+        try {
+            lock.writeLock().lock();
+            amount -= fuel;
+        } finally {
+            lock.writeLock().unlock();
+        }
+    }
+
+    private boolean notEnough(float fuel) {
+        System.out.println("How much fuel? " + amount + " liters");
+        try {
+            lock.readLock().lock();
+            return amount < fuel;
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 }
